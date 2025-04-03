3a5d598a50e81be578f704f4abeefca52cafcb01
JVCarv41 <jvc.04.off@gmail.com>
2025-04-03 13:42:17 -0300

    Added input fields
    Added code to create the input fields in the future, including the input component and its style in a separated CSS file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import InputField from "./components/InputField/InputField";
 
 function App() {
+  // const name: string = "Name";
   return (
     <div className="App">
       <header>
         <title>RPGen - DnD Character Sheet Generator</title>
-        <link rel="icon" href="./favicon.webp"/>
+        <link rel="icon" href="./favicon.webp" />
       </header>
       <body className="App">
         <div className="App-header">
@@ -14,6 +16,8 @@ function App() {
         </div>
         <div className="char-sheet">
           <h4>Character Sheet</h4>
+          <InputField input_text={"Name"}/>
+          <InputField input_text={"Class"}/>
         </div>
       </body>
     </div>
